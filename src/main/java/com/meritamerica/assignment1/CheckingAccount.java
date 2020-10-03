aadf45851ea67b7d2f2ca453555aa207a073ceca
Shansnid12 <shansnid12@gmail.com>
2020-10-03 15:02:19 -0500

    Accounts

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -4,6 +4,7 @@ import java.lang.Math;
 
 import java.text.NumberFormat;
 
+//Creates Savings Account Class and methods computing withdawals, deposits and future value calculated with interest
 public class CheckingAccount {
 	private double balance;
 	private double interestRate;
@@ -25,6 +26,8 @@ public class CheckingAccount {
 		return this.interestRate;
 	}
 	
+	/*This will return a true or false - withdraw amount cannot be negative or more than is in Checking Account Balance.
+	  This also subtracts the withdraw amount from Savings Account Balance if true*/
 	public boolean withdraw(double amount) {
 		if (amount < 0 || amount > this.balance) {
 			return false;
@@ -34,6 +37,8 @@ public class CheckingAccount {
 		}
 	}
 	
+	/*This will return a true or false - deposit amount cannot be negative. This also adds the deposit amount to Checking
+	  Account Balance if true*/
 	public boolean deposit(double amount) {
 		if (amount < 0) {
 			return false;
@@ -43,22 +48,25 @@ public class CheckingAccount {
 		}
 	}
 	
+	//futureValue method calculates future value of Checking Account
 	public double futureValue(int years) {
 		return this.balance * Math.pow(1 + this.interestRate, years);
 	}
 
+	//toString method specifies the format for digits to the right of the decimal and the information to print
 	public String toString() {
 		NumberFormat defaultFormat = NumberFormat.getInstance();
 		defaultFormat.setMinimumFractionDigits(2);
 		
-		return "Checking Account Balance: " + balance + "\n" + "Checking Account Interest Rate: " + (this.interestRate)*100 + "%"
+		return "Checking Account Balance: $" + defaultFormat.format(balance) + "\n" + "Checking Account Interest Rate: " + (this.interestRate)*100 + "%"
 				+ "\n" + "Checking Account Balance in " + years + " years: $" + defaultFormat.format(futureValue(this.years));
 
 	}
-		
+	
+	//Creates CheckingAccount Object of the amount of $100
 	public static void main(String[] args) {
 		
-		CheckingAccount checkingAccount = new CheckingAccount(1000);
+		CheckingAccount checkingAccount = new CheckingAccount(100);
 		System.out.println(checkingAccount);	
 		
 	}
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -4,6 +4,7 @@ import java.lang.Math;
 
 import java.text.NumberFormat;
 
+//Creates Savings Account Class and methods computing withdawals, deposits and future value calculated with interest
 public class SavingsAccount {
 	private double balance;
 	private double interestRate;
@@ -25,6 +26,8 @@ public class SavingsAccount {
 		return this.interestRate;
 	}
 
+	/*This will return a true or false - withdraw amount cannot be negative or more than is in Savings Account Balance.
+	  This also subtracts the withdraw amount from Savings Account Balance if true*/
 	public boolean withdraw(double amount) {
 		if (amount < 0 || amount > this.balance) {
 			return false;
@@ -34,6 +37,8 @@ public class SavingsAccount {
 		}
 	}
 
+	/*This will return a true or false - deposit amount cannot be negative. This also adds the deposit amount to Savings
+	  Account Balance if true*/
 	public boolean deposit(double amount) {
 		if (amount < 0) {
 			return false;
@@ -43,19 +48,22 @@ public class SavingsAccount {
 		}
 	}
 
+	//futureValue method calculates future value of Savings Account
 	public double futureValue(int years) {
 		return this.balance * Math.pow(1 + this.interestRate, years);
 	}
 
+	//toString method specifies the format for decimal places and what we information to print
 	public String toString() {
 		NumberFormat defaultFormat = NumberFormat.getInstance();
 		defaultFormat.setMinimumFractionDigits(2);
 		
-		return "Savings Account Balance: " + balance + "\n" + "Savings Account Interest Rate: " + (this.interestRate)*100 + "%" +
+		return "Savings Account Balance: $" + defaultFormat.format(balance) + "\n" + "Savings Account Interest Rate: " + (this.interestRate)*100 + "%" +
 				"\n" + "Savings Account Balance in " + years + " years: $" + defaultFormat.format(futureValue(this.years));
 
 	}
 	
+	//Creates SavingsAccount Object of the amount of $1,000
 	public static void main(String[] args) {
 		
 		SavingsAccount savingsAccount = new SavingsAccount(1000);
